34176975a98f238e74e7f2118c5e9169a6afada9
Mark Makondo <markalbert.makondo@gmail.com>
2021-07-14 23:33:05 +0800

    first commit

---
+++ src/components/app/AppContainer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// ui
+import App from './App';
+
+const AppContainer = () => {
+  return <App />;
+};
+
+export default AppContainer;
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// style
+import 'scss/global.scss';
+
+// component
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
